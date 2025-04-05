bd4e6780ce3892fbdf8604f9af2cc1fe5af99971
Jo HyunA <teniuscom@gmail.com>
2025-04-05 16:42:44 +0900

    [feat] 카카오 로그인 Callback API 수정
    프론트에서 GET api/oauth/kakao/callback의 응답을 받은 후 메인 페이지로 이동할 방법이 없어서, 백엔드 단에서 302 Found와 함께 Location 헤더를 설정해주기로 했습니다.
    
    카카오 간편 로그인이 성공하면 메인 페이지로 리다이렉트하고, 실패하면 500 에러 페이지로 리다이렉트합니다.

--- src/main/java/com/example/plango/auth/oauth/kakao/controller/KakaoOAuthController.java
+++ src/main/java/com/example/plango/auth/oauth/kakao/controller/KakaoOAuthController.java
@@ -6,7 +6,6 @@ import com.example.plango.auth.oauth.service.OAuthService;
 import com.example.plango.common.dto.SuccessResponse;
 import com.example.plango.common.url.UrlManager;
 import com.example.plango.common.url.UrlPath;
-import com.example.plango.user.dto.UserProfileReadResponseDTO;
 import com.example.plango.auth.jwt.service.JwtManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,6 +17,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 
 @RestController
@@ -42,7 +43,7 @@ public class KakaoOAuthController {
      *          (body) 카카오 로그인 URL
      */
     @GetMapping("/login")
-    public ResponseEntity<String> redirectToKakaoAuth(){
+    public ResponseEntity<SuccessResponse> redirectToKakaoAuth(){
         // 카카오 로그인 화면으로 리다이렉트
         String kakaoLoginUrl=urlManager.getUrl(UrlPath.KAKAO_LOGIN_URL);
         String kakaoLoginRedirectUri=urlManager.getUrl(UrlPath.KAKAO_LOGIN_REDIRECT_URI);
@@ -52,7 +53,7 @@ public class KakaoOAuthController {
                 .append("&response_type=code")
                 .toString();
 
-        return ResponseEntity.status(HttpStatus.OK).body(url);
+        return ResponseEntity.status(HttpStatus.OK).body(SuccessResponse.of(Map.of("redirectUrl", url)));
    }
 
     /**
@@ -60,32 +61,46 @@ public class KakaoOAuthController {
      * (인가 코드를 받아 로그인 처리를 완료시킴)
      *
      * @param code 인가 코드
-     * @return  (status) 200,
-     *          (header > Authorization) Access 토큰
+     * @return  (status) 302,
+     *          (header > Location) 성공한 경우 -> 프론트 메인 페이지 URL
+     *          (header > Location) 실패한 경우 -> 프론트 500 에러 페이지 URL
      *          (header > Set-Cookie) Refresh 토큰
-     *          (body) 사용자 프로필
      */
     @GetMapping("/callback")
     public ResponseEntity<SuccessResponse> callback(@RequestParam String code){
-        // 유저 프로필 조회
-        UserProfileReadResponseDTO userProfileDTO=oAuthService.loginWithKakao(code);
+        try{
+            // 카카오 로그인
+            String userId=oAuthService.loginWithKakao(code);
 
-        // HTTP Header 설정
-        HttpHeaders headers=new HttpHeaders();
+            // HTTP Header 설정
+            HttpHeaders headers=new HttpHeaders();
 
-        // Access 토큰 발급
-        String userId= userProfileDTO.getId();
-        String accessToken = jwtManager.generateAccessToken(userId);
-        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
+            // Location 헤더 설정 (성공했으면, 메인 페이지로 리다이렉트)
+            String frontMainPageUrl=urlManager.getUrl(UrlPath.FRONT_MAIN_PAGE);
+            headers.add(HttpHeaders.LOCATION, frontMainPageUrl);
 
-        // Refresh 토큰 발급
-        ResponseCookie refreshTokenCookie= jwtManager.generateRefreshCookie(userId);
-        headers.add(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
+            // Refresh 토큰 발급
+            ResponseCookie refreshTokenCookie= jwtManager.generateRefreshCookie(userId);
+            headers.add(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
 
-        // DB에 Refresh 토큰 갱신
-        String refreshToken=refreshTokenCookie.getValue();
-        userRefreshTokenService.createOrUpdate(userId, refreshToken);
+            // DB에 Refresh 토큰 갱신
+            String refreshToken=refreshTokenCookie.getValue();
+            userRefreshTokenService.createOrUpdate(userId, refreshToken);
 
-        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(SuccessResponse.of(userProfileDTO));
+            return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+        } catch (Exception e){
+            // 실패 했으면, 500 에러 페이지로 리다이렉트
+
+            HttpHeaders headers=new HttpHeaders();
+
+            // Location 설정
+            String front500ErrorPageUrl= urlManager.getUrl(UrlPath.FRONT_500_ERROR_PAGE);
+            headers.add(HttpHeaders.LOCATION, front500ErrorPageUrl);
+
+            return ResponseEntity.status(HttpStatus.FOUND)
+                    .headers(headers)
+                    .build();
+
+        }
     }
 }
--- src/main/java/com/example/plango/auth/oauth/service/OAuthService.java
+++ src/main/java/com/example/plango/auth/oauth/service/OAuthService.java
@@ -1,7 +1,5 @@
 package com.example.plango.auth.oauth.service;
 
-import com.example.plango.user.dto.UserProfileReadResponseDTO;
-
 public interface OAuthService {
-    UserProfileReadResponseDTO loginWithKakao(String code);
+    String loginWithKakao(String code);
 }
